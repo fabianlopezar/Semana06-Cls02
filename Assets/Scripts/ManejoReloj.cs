8e09d790cb57a660b8276a25822152bc5750fcde
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2023-09-06 19:09:17 -0500

    Initial commit

---
+++ Assets/Scripts/ManejoReloj.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ManejoReloj : MonoBehaviour
+{
+
+    public TMP_Text textMin;
+    public TMP_Text textSeg;
+    public TMP_Text textMilS;
+
+    private float startTime;
+    private float stopTime;
+    private float timerTime;
+    private bool isRunning=false;
+
+ 
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
